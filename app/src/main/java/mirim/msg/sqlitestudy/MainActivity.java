fb6b40b816b6f2d6fd115a75bde144bfb10f04f6
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-01-27 13:35:31 +0900

    table 생성

--- app/src/main/java/mirim/msg/sqlitestudy/MainActivity.java
+++ app/src/main/java/mirim/msg/sqlitestudy/MainActivity.java
@@ -2,6 +2,9 @@ package mirim.msg.sqlitestudy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public class myDBHelper extends SQLiteOpenHelper{
+        public myDBHelper(Context context){
+            super(context,"groupDB",null,1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            db.execSQL("CREATE TABLE groupTBL (gName CHAR(20) PRIMARY KEY, gNumber INTEGER);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            db.execSQL("DROP TABLE IF EXISTS groupTBL");
+            onCreate(db);
+        }
     }
 }
